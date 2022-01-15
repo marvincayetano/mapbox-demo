e203773e156bddf0d7e9d25f462399621578437e
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-01-14 22:04:51 -0500

    🔥 Chore: Try push try push

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,24 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div>asdf</div>;
+  return (
+    <div className="min-w-32 bg-white min-h-48 p-3 mb-4 font-medium">
+      <div className="w-32 flex-none rounded-t lg:rounded-t-none lg:rounded-l text-center shadow-lg ">
+        <div className="block rounded-t overflow-hidden  text-center ">
+          <div className="bg-blue-500 text-white py-1">March</div>
+          <div className="pt-1 border-l border-r border-white bg-white">
+            <span className="text-5xl font-bold leading-tight">17</span>
+          </div>
+          <div className="border-l border-r border-b rounded-b-lg text-center border-white bg-white -pt-2 -mb-1">
+            <span className="text-sm">Sunday</span>
+          </div>
+          <div className="pb-2 border-l border-r border-b rounded-b-lg text-center border-white bg-white">
+            <span className="text-xs leading-normal">8:00 am to 5:00 pm</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
